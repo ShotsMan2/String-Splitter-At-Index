af4c8f747bdd362d6cb51d4509e1a0c18d0a3ced
Selim Efe <uysalselimefe@gmail.com>
2026-01-17 21:58:57 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,39 +4,38 @@
 
 int main()
 {
-    char str[99],str1[99],str2[99];
-    int len=0,i=0,n,j;
+    char str[99], str1[99], str2[99];
+    int len = 0, i = 0, n, j;
 
     printf("str girin ");
-    scanf("%s",&str);
+    scanf("%s", &str);
 
     printf("\n n i girin ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    while(str[i]!='\0'){
+    while (str[i] != '\0')
+    {
         len++;
         i++;
     }
 
-    for(i=0;i<n;i++){
-        str1[i]=str[i];
+    for (i = 0; i < n; i++)
+    {
+        str1[i] = str[i];
     }
-    str1[n]='\0';
+    str1[n] = '\0';
 
-    j=0;
+    j = 0;
 
-    for(i=n;i<len;i++){
-        str2[j]=str[i];
+    for (i = n; i < len; i++)
+    {
+        str2[j] = str[i];
         j++;
     }
 
-    str2[j+1]='\0';
+    str2[j + 1] = '\0';
 
+    printf("str1= %s \n", str1);
 
-
-    printf("str1= %s \n",str1);
-
-
-
-    printf("str2= %s \n",str2);
+    printf("str2= %s \n", str2);
 }
